00ef49d24f83ac9beb0eed2853843ff0f0fcef68
patd <pdoran@gmail.com>
2020-12-16 22:26:12 -0500

    Add Timeline with React.

--- patrickdoran_reacttimeline/src/App.js
+++ patrickdoran_reacttimeline/src/App.js
@@ -1,28 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from 'react';
+
 import './timeline.css';
-import {timeline} from './timeline.js';
+import { timeline } from './timeline.js';
 
 
-timeline(document.querySelectorAll('.timeline'));
+let wordPressGraphQL = 'https://patrickdoran.com/headless/graphql'
 
 
+function buildTimeline(){
+  useEffect(() => {
+ 
+    console.log('lifecycle')
+  }, []);
 
-class App extends Component {
+}
+
+
+class PatrickDoranTimeline extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+
+    fetch(wordPressGraphQL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          {
+          posts {
+            edges {
+              node {
+                excerpt
+                date
+                postId
+                title
+                content
+              }
+            }
+          }
+          }
+        `,
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data.data.posts.edges);
+        let results = data.data.posts.edges
+        this.setState({ results, isLoading: false })
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+  }
+
+  
+
+  componentDidUpdate() {
+    const { results } = this.state;
+
+    if (results.length > 0) {
+      timeline(document.querySelectorAll('.timeline'));
+    }
+  }
+  
+
+  
+
   render() {
+    const { results, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='timeline' data-mode='horizontal' data-rtl-mode='true' data-force-vertical-mode='300'>
+        <div className='timeline__wrap'>
+          <div className='timeline__items'>
+            {results.map(result =>
+              <div className='timeline__item' key={result.node.postId}>
+                <div className='timeline__content'>
+                  {result.node.title}</div>
+              </div>
+            )}
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default PatrickDoranTimeline;
